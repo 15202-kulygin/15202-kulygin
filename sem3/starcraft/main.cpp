3a53f9e8d5f9b5c4e0f4587c8dc7677bc9e0bd8b
Max <maxsham@ngs.ru>
2016-10-07 15:57:24 +0700

    qwe

--- sem3/starcraft/main.cpp
+++ sem3/starcraft/main.cpp
@@ -2,11 +2,26 @@
 #include "battlecruiser.h"
 #include "zergling.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+/*Unit * createUnit (const string& id)
+{
+	if ("battlecruiser" == id)
+	{
+		return new BattleCruiser;
+	}
+	else if ("zergling" == id)
+	{
+		return new Zergling;
+	}
+}*/
+
 int main ()
 {
+	string unit_id = "battlecruiser";
+
 	Unit * bc = new BattleCruiser;
 	bc->move(666, 77);
 	delete bc;
